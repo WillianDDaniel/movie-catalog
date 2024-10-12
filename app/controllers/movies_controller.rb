8dbcae89ec86cfc4e3843ca693c550345c16694d
WillianDDaniel <williandeivitidaniel@live.com>
2024-10-12 01:08:17 -0300

    style: added new style to new_movie, new_didirector and new_genre pages

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -2,11 +2,15 @@ class MoviesController < ApplicationController
   def index
     @movies = Movie.where(isSketch: false)
   end
+
   def show
     @movie = Movie.find(params[:id])
   end
+
   def new
     @movie = Movie.new
+    @genres = Genre.all
+    @directors = Director.all
   end
 
   def create
@@ -35,6 +39,11 @@ class MoviesController < ApplicationController
     if @movie.delete
       redirect_to movies_path
     end
+  end
+
+  private
+  def movie_params
+    params.require(:movie).permit(:title, :year, :country, :synopsis, :isSketch, :duration, :director_id, :genre_id, :cover_url)
   end
 
 end
